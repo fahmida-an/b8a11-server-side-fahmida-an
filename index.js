25d5571a391ed28bf65cf79996d3e90858cf907a
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-11-07 22:06:16 +0600

    Added delete, patch, logger and verify token

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors());
+//middlewares
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pdaizgq.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,17 +25,59 @@ const client = new MongoClient(uri, {
   },
 });
 
+
+//middleware made
+const logger = async(req, res, next) => {
+  console.log('called:', req.host, req.originalUrl);
+  next();
+}
+
+const verifyToken = async(req, res, next) => {
+  const token = req.cookies?.token;
+  console.log('value of token in middleware: ', token);
+  if(!token){
+    return res.status(401).send({message: 'not authorized'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decoded) =>{
+    //error
+    if(err){
+      console.log(err);
+      return res.status(401).send({message: 'unauthorized'})
+    }
+    // valid decoded
+    console.log('value in the token', decoded);
+    req.user = decoded;
+    next()
+  })
+ 
+}
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     const serviceCollection = client.db("techGadgets").collection("services");
-    const bookServiceCollection = client
-      .db("techGadgets")
-      .collection("bookServices");
+    const bookServiceCollection = client.db("techGadgets").collection("bookServices");
 
-    app.get("/services", async (req, res) => {
+    //auth api
+
+    app.post('/jwt', logger, async(req,res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false
+      })
+      .send({success: true})
+    })
+
+    //service api
+    app.get("/services", logger, async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -48,8 +97,10 @@ async function run() {
     });
 
     //get bookservice data with user email
-    app.get("/bookServices", async (req, res) => {
+    app.get("/bookServices", logger, verifyToken, async (req, res) => {
       console.log(req.query.email);
+      // console.log('token', req.cookies.token);
+      console.log('user in valid token',req.user);
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
@@ -57,6 +108,30 @@ async function run() {
       const result = await bookServiceCollection.find(query).toArray();
       res.send(result);
     });
+
+    app.patch('/bookServices/:id', async(req,res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedBookServices = req.body;
+      console.log(updatedBookServices);
+      const updatedDoc = {
+        $set: {
+          status: updatedBookServices.status
+        }
+      }
+
+      const result = await bookServiceCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+
+    })
+
+    app.delete('/bookServices/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await bookServiceCollection.deleteOne(query);
+      res.send(result)
+    })
+
 
     
 
